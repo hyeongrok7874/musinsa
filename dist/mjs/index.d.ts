05db134c69ed6437a2f82d10f01fc8e8d71ad2b6
hyeongrok7874 <khr5k0228@gmail.com>
2023-01-20 17:42:12 +0900

    Update dist

--- dist/mjs/index.d.ts
+++
@@ -1,8 +0,0 @@
-export interface DailyRankingType {
-    img: string;
-    brand: string;
-    name: string;
-    price: string;
-    link: string;
-}
-export declare const getDailyRanking: () => Promise<DailyRankingType[] | []>;
