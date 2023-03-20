18c1b14991c4dec5e76ae0030cfedf9bb573c8d7
hyeongrok7874 <khr5k0228@gmail.com>
2023-03-20 14:17:06 +0900

    Add new function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import cheerio from "cheerio";
 
-const MUSINSA_DAILY = "https://www.musinsa.com/ranking/best?period=day";
 const MUSINSA_NOW = "https://www.musinsa.com/ranking/best?period=now";
+const MUSINSA_DAILY = "https://www.musinsa.com/ranking/best?period=day";
 const MUSINSA_WEEK = "https://www.musinsa.com/ranking/best?period=week";
 const MUSINSA_MONTH = "https://www.musinsa.com/ranking/best?period=month";
 const MUSINSA_MONTH_3 = "https://www.musinsa.com/ranking/best?period=month_3";
@@ -14,7 +14,7 @@ const GET_PRICE = "div.article_info p.price";
 
 const priceFilter = (price: string) => price.split("\n")[2].replace(/\s/g, "");
 
-export interface DailyRankingType {
+export interface RankingType {
   img: string;
   brand: string;
   name: string;
@@ -22,14 +22,14 @@ export interface DailyRankingType {
   link: string;
 }
 
-export const getDailyRanking = async (): Promise<DailyRankingType[] | []> => {
+const getRanking = async (url: string): Promise<RankingType[] | []> => {
   try {
-    const { data } = await axios.get(MUSINSA_DAILY);
+    const { data } = await axios.get(url);
     const $ = cheerio.load(data);
     const body = $("div.li_inner");
-    let dailyRanking: DailyRankingType[] = [];
+    let ranking: RankingType[] = [];
     body.map((i, item) => {
-      dailyRanking[i] = {
+      ranking[i] = {
         img: $(item).find(GET_IMG).attr("data-original") ?? "",
         brand: $(item).find(GET_BRAND).text() ?? "",
         name: $(item).find(GET_NAME_AND_LINK).attr("title") ?? "",
@@ -37,8 +37,15 @@ export const getDailyRanking = async (): Promise<DailyRankingType[] | []> => {
         link: $(item).find(GET_NAME_AND_LINK).attr("href") ?? "",
       };
     });
-    return dailyRanking.length > 0 ? dailyRanking : [];
+    console.log(ranking);
+    return ranking.length > 0 ? ranking : [];
   } catch (e) {
     throw e;
   }
 };
+
+export const getNowRanking = () => getRanking(MUSINSA_NOW);
+export const getDailyRanking = () => getRanking(MUSINSA_DAILY);
+export const getWeeklyRanking = () => getRanking(MUSINSA_WEEK);
+export const getMonthlyRanking = () => getRanking(MUSINSA_MONTH);
+export const get3MonthlyRanking = () => getRanking(MUSINSA_MONTH_3);
